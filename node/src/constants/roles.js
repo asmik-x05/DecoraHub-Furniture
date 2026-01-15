74ec85679ae4b386b659554a5acf172296a1acc2
Asmik Lamichhane <asmiklmc.x05@gmail.com>
2026-01-15 16:09:33 +0545

    add authentication & role based authorization

---
+++ node/src/constants/roles.js
@@ -0,0 +1,4 @@
+export const ROLE_USER = "USER";
+export const ROLE_MERCHANT = "MERCHANT";
+export const ROLE_ADMIN = "ADMIN";
+
--- node/src/routes/product.routes.js
+++ node/src/routes/product.routes.js
@@ -1,12 +1,33 @@
+import auth from "../middlewares/auth.js";
+import authorizeRole from "../middlewares/roleBasedAuth.js";
 import express from "express";
 import productController from "../controllers/product.controller.js";
+import { ROLE_MERCHANT } from "../constants/roles.js";
 
 const router = express.Router();
 
-router.delete("/:id", productController.deleteProduct);
+router.delete(
+  "/:id",
+  auth,
+  authorizeRole(ROLE_MERCHANT),
+  productController.deleteProduct
+);
+
 router.get("/", productController.getProduct);
 router.get("/:id", productController.getProductsById);
-router.post("/", productController.createProduct);
-router.put("/:id", productController.updateProduct);
+
+router.post(
+  "/",
+  auth,
+  authorizeRole(ROLE_MERCHANT),
+  productController.createProduct
+);
+
+router.put(
+  "/:id",
+  auth,
+  authorizeRole(ROLE_MERCHANT),
+  productController.updateProduct
+);
 
 export default router;

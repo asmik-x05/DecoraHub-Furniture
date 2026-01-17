cf9737eb4ae4cffa975adf461bd5f5a9cc77e93c
Asmik Lamichhane <asmiklmc.x05@gmail.com>
2026-01-17 20:27:21 +0545

    add validation for order creation

---
+++ node/src/libs/schemas/order.js
@@ -0,0 +1,20 @@
+import z from "zod";
+
+const addressSchema = z.object({
+  country: z.string(),
+  city: z.string(),
+  province: z.string(),
+  street: z.string(),
+});
+
+const orderSchema = z.object({
+  orderIteam: z.array(
+    z.object({
+      product: z.string({ error: "product is required" }),
+    })
+  ),
+  totalPrice: z.number({ error: "total price is required" }).min(0),
+  shippingAddress: addressSchema,
+});
+
+export { orderSchema };
